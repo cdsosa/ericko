7de036539cdd2c2cb5aaec24de1307520ed93ce7
erick <erickolivares21@gmail.com>
2015-11-21 12:53:37 -0700

    updated footer and porfolio hovers

--- wp-content/themes/amazingerick/footer.php
+++ wp-content/themes/amazingerick/footer.php
@@ -9,7 +9,7 @@
 <footer>
     <div class="grid">
 
-        <p>&copy; 2015 All rights reserved Erick Olivares</p>
+        <p>&copy; 2015 Erick Olivares&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Provo, Utah</p>
 
         <div class="social-icons">
             <a href="https://www.behance.net/erickolivares" target="_blank">
@@ -17,9 +17,6 @@
             </a>
             <a href="https://github.com/erickolivares" target="_blank">
                 <i class="fa fa-github fa-lg"></i>
-            </a>
-            <a href="https://www.facebook.com/Salvy102" target="_blank">
-                <i class="fa fa-facebook fa-lg"></i>
             </a>
             <a href="https://twitter.com/EricksEmpire" target="_blank">
                 <i class="fa fa-twitter fa-lg"></i>
@@ -30,7 +27,7 @@
             <a href="https://www.linkedin.com/profile/view?id=113044967&trk=nav_responsive_tab_profile" target="_blank">
                 <i class="fa fa-linkedin fa-lg"></i>
             </a>
-            <a href="mailto:erickolivares21@gmail.com?subject=Flip my Design Around!&body=Hello Erick, Can you design my website... logo... ">
+            <a href="mailto:erickolivares21@gmail.com?subject=ErickOlivares.com">
                 <i class="fa fa-envelope fa-lg"></i>
             </a>
         </div>
