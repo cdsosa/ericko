0d1e26214bbbf1512129a79a2885ce0cabae42b2
erick <erickolivares21@gmail.com>
2016-01-10 18:05:51 -0700

    changed theme layouts and styles

--- wp-content/themes/amazingerick/template-parts/services-module.php
+++
@@ -1,8 +0,0 @@
-<?php ?>
-<div class="services slideImage black-overlay" style="background-image: url('<?php echo get_template_directory_uri();?>/img/backgrounds/mountains.jpg')">
-    <h5 class="tab">Services</h5>
-    <img class="main-target" src="<?php echo get_template_directory_uri();?>/img/services/all-services.png" />
-    <a href="/services/">
-        <button class="reversed mod-fixed">Learn More</button>
-    </a>
-</div>
